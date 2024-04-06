a866521882880a89d87364b8730c5fca7d5f546a
Quasar_0330 <95518809+Quasar-0330@users.noreply.github.com>
2024-04-07 04:53:57 +0900

    Create init.lua

---
+++ .config/nvim/init.lua
@@ -0,0 +1,43 @@
+vim.opt.encoding = 'utf-8'
+vim.opt.number = true
+vim.opt.title = true
+vim.opt.laststatus = 2
+vim.opt.ruler = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.matchtime = 1
+vim.opt.showmatch = true
+vim.opt.backup = false
+vim.opt.swapfile = false
+vim.opt.showcmd = true
+vim.opt.autoread = true
+vim.opt.incsearch = true
+vim.opt.smartindent = true
+vim.opt.autoindent = true
+vim.opt.scrolloff = 5
+vim.opt.sidescrolloff = 5
+vim.opt.backspace = 'indent,eol,start'
+vim.opt.syntax = 'on'
+vim.opt.mouse = ''
+vim.opt.hlsearch = false
+
+require('plugins')
+
+vim.g.indentLine_char = '┊'
+
+require('onenord').setup()
+require('nvim-autopairs').setup()
+require('tidy').setup()
+require('ibl').setup()
+require('colorizer').setup()
+require('lualine').setup {
+    option = {
+        theme = 'onenord'
+    }
+}
+
+if vim.fn.executable('fcitx5') then
+    vim.cmd([[autocmd InsertLeave * :silent !fcitx5-remote -c]])
+    vim.cmd([[autocmd CmdlineLeave * :silent !fcitx5-remote -c]])
+end
