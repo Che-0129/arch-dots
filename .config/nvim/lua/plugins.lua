eb82a7cc6ef3a4e9aa03cca369afb895c4830bb7
Quasar_0330 <95518809+Quasar-0330@users.noreply.github.com>
2024-04-07 04:55:15 +0900

    Create plugins.lua

---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,11 @@
+vim.cmd.packadd "packer.nvim"
+require("packer").startup(function()
+    use {"wbthomason/packer.nvim", opt = true}
+    use "nvim-lualine/lualine.nvim"
+    use "rmehri01/onenord.nvim"
+    use "neoclide/coc.nvim"
+    use "windwp/nvim-autopairs"
+    use "mcauley-penney/tidy.nvim"
+    use "lukas-reineke/indent-blankline.nvim"
+    use "Norcalli/nvim-colorizer.lua"
+end)
