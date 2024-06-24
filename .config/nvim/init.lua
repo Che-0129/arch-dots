77ae8ebf55ce9bcb6120f029cc56d9d7ecf7cc92
Quasar_14A <95518809+Quasar-0330@users.noreply.github.com>
2024-06-24 16:02:40 +0900

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -55,6 +55,8 @@ require("lazy").setup({
     "neovim/nvim-lspconfig",
     "hrsh7th/nvim-cmp",
     "hrsh7th/cmp-nvim-lsp",
+    "hrsh7th/cmp-path",
+    "hrsh7th/cmp-buffer",
     "saadparwaiz1/cmp_luasnip",
     "L3MON4D3/LuaSnip",
     "williamboman/mason.nvim",
@@ -124,5 +126,7 @@ cmp.setup {
   sources = {
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
+    { name = 'buffer' },
+    { name = 'path' },
   },
 }
