d8f8172fa666e247a6565282ec7be32fcd0d0d50
Quasar_14A <95518809+Quasar-0330@users.noreply.github.com>
2024-07-23 17:32:00 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,16 @@
-mv 
+#!/bin/bash
+mv -f ./config/* ~/.config/
+bash ./xremap-setup.sh
+sudo cat << 'EOT' | sudo tee /etc/systemd/system/disable-USB-wakeup.service
+[Unit]
+Description=Disable USB wakeup triggers in /proc/acpi/wakeup
+
+[Service]
+Type=oneshot
+ExecStart=/bin/sh -c "echo XHC0 > /proc/acpi/wakeup"
+ExecStop=/bin/sh -c "echo XHC0 > /proc/acpi/wakeup"
+RemainAfterExit=yes
+
+[Install]
+WantedBy=multi-user.target
+EOT
