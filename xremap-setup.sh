88a6a13cb13a73453f3c9ea283bbcb51505c71bc
Quasar_14A <95518809+Quasar-0330@users.noreply.github.com>
2025-07-01 20:04:04 +0900

    Update xremap-setup.sh

--- xremap-setup.sh
+++ xremap-setup.sh
@@ -2,4 +2,3 @@
 sudo gpasswd -a $(whoami) input
 echo 'uinput' | sudo tee /etc/modules-load.d/uinput.conf
 echo 'KERNEL=="uinput", GROUP="input", TAG+="uaccess"' | sudo tee /etc/udev/rules.d/99-input.rules
-systemctl --user enable /home/yuto/.config/xremap/systemd/xremap.service
