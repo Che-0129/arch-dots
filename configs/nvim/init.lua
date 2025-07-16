109bfa05a2d6f586a5310d8306e850ac10f0c837
Quasar_14A <95518809+Quasar-0330@users.noreply.github.com>
2025-07-16 13:15:26 +0900

    Update init.lua

--- configs/nvim/init.lua
+++ configs/nvim/init.lua
@@ -181,3 +181,8 @@ vim.api.nvim_create_autocmd("BufWritePre", {
     pattern = "*",
     command = "retab"
 })
+
+vim.api.nvim_create_autocmd("VimLeave", {
+  pattern = "*",
+  command = "set guicursor=a:ver25",
+})
