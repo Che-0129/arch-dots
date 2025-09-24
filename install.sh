db2aa6b9660ca9c272c002a4930685f9df482d4a
Che_0129 <95518809+Che-0129@users.noreply.github.com>
2025-09-24 09:03:12 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,17 +2,7 @@
 
 rm -rf ~/.config/fish
 mv $(cd $(dirname $0) && pwd)/configs/* ~/.config/
-sudo mv $(cd $(dirname $0) && pwd)/nwg-hello/* /etc/nwg-hello/
 bash $(cd $(dirname $0) && pwd)/xremap-setup.sh
-
-sudo cat << 'EOT' | sudo tee /etc/greetd/config.toml
-[terminal]
-vt = 1
-
-[default_session]
-command = "Hyprland -c /etc/nwg-hello/hyprland.conf"
-user = "greeter"
-EOT
 
 sudo cat << 'EOT' | sudo tee /etc/systemd/system/disable-USB-wakeup.service
 [Unit]
@@ -28,5 +18,5 @@ RemainAfterExit=yes
 WantedBy=multi-user.target
 EOT
 
-sudo systemctl enable {disable-USB-wakeup,greetd}.service
+sudo systemctl enable disable-USB-wakeup.service
 ya pkg add {ndtoan96/ouch,yazi-rs/plugins:full-border}
